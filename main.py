252a406c4ae6353cc3de389dfe5506c6c3008d89
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-25 04:08:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,25 @@ st.subheader("Это подзаголовок")
 # Текст
 st.text("Просто текст")
 
+
+# полоски
+st.divider()
+st.divider()
+
+st.success("Success")
+st.info("Information")
+st.warning("Warning")
+st.error("Error")
+# st.snow()
+# st.balloons()
+
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+
