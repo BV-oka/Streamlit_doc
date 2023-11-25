268ac027a5c7a9218fe84328c5abdb2075934372
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-25 04:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+if st.button('Нажми на меня'):
+  st.write('Привет, мир!')
+
+
+# Название
+st.title("Песочница")
+
+# Заголовок
+st.header("Это заголовок")
+
+# Подзаголовок
+st.subheader("Это подзаголовок")
+
+# Текст
+st.text("Просто текст")
 
